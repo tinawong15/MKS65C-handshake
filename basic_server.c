06433d6ee51a1613ce3ddfec565ecf7c4e70d586
tina <tinaw425@gmail.com>
2018-12-19 18:23:58 -0500

    added new work

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,33 @@
 #include "pipe_networking.h"
 
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    printf("Removing well known pipe...\n");
+    remove("server");
+    printf("Server is exiting...\n");
+    exit(0);
+  }
+}
 
 int main() {
+  signal(SIGINT, sighandler);
 
   int to_client;
   int from_client;
+  char msg[BUFFER_SIZE];
+  int i;
 
-  from_client = server_handshake( &to_client );
+  while(1) {
+    from_client = server_handshake( &to_client );
+
+    while(read(from_client, msg, BUFFER_SIZE)) {
+      printf("Server received: %s\n", msg);
+      for(i = 0; i < strlen(msg); i++) {
+        msg[i] += 1;
+      }
+      write(to_client, msg, BUFFER_SIZE);
+      printf("Server sent: %s\n", msg);
+    }
+    printf("Client disconnected. Time for a new handshake!\n");
+  }
 }
--- client.c
+++ client.c
@@ -1,10 +1,29 @@
 #include "pipe_networking.h"
 
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    printf("Client is exiting...\n");
+    exit(0);
+  }
+}
 
 int main() {
+  signal(SIGINT, sighandler);
 
   int to_server;
   int from_server;
+  char msg[BUFFER_SIZE];
 
   from_server = client_handshake( &to_server );
+
+  while(1) {
+    printf("User input to server: ");
+    fgets(msg, BUFFER_SIZE, stdin);
+    msg[strlen(msg)-1] = '\0';
+    printf("Client sent: %s\n", msg);
+
+    write(to_server, msg, BUFFER_SIZE);
+    read(from_server, msg, BUFFER_SIZE);
+    printf("Client received: %s\n", msg);
+  }
 }
